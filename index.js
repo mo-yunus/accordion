c29dbd1e4ba37530b08ff5ea1c6680fcf1371bfc
yunus <yunusmomoh65@gmail.com>
2022-07-22 11:22:48 +0000

    accordion built without framework

---
+++ index.js
@@ -0,0 +1,17 @@
+let accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function () {
+        this.classList.toggle('is-open');
+        let content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            //accordion is open, we need to close it
+            content.style.maxHeight = null;
+
+        } else {
+            //accordion is closed
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
